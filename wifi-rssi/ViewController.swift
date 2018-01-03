da8e04c8140f079ca577744b765c593f342ca1cb
Zhihui Tang <crafttang@gmail.com>
2018-01-03 15:38:48 +0100

    Initial Commit

---
+++ wifi-rssi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wifi-rssi
+//
+//  Created by Zhihui Tang on 2018-01-03.
+//  Copyright © 2018 Zhihui Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
